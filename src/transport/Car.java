cc22a1b6a54ecc5a28ae51e29eeedc6b9ced40a8
Evgenia Ganich <evgeniya.bw@gmail.com>
2022-10-23 17:41:27 +0300

    ДЗ не получается!

--- src/transport/Car.java
+++ src/transport/Car.java
@@ -18,50 +18,65 @@ public class Car {
     private Key key;
     private Insurance insurance;
 
-    public Car(String brand, String model, double engineVolume,
-               String color, int productionYear, String productionCountry, new Key(), new Insurance()) {
 
-        if (brand == null) {
-            this.brand = "default";
-        } else {
-            this.brand = brand;
-        }
+    public Car(String brand, String model, double engineVolume,
+               String color, int productionYear, String productionCountry) {
+        this(
+                brand,
+                model,
+                engineVolume,
+                color,
+                productionYear,
+                productionCountry,
+                "МКПП",
+                "х000хх000",
+                "седан",
+                5,
+                true
+            new Key(),
+            new Insurance()
+        );
+    }
+     if (brand == null) {
+        this.brand = "default";
+    } else {
+        this.brand = brand;
+    }
         if (model == null) {
-            this.model = "default";
-        } else {
-            this.model = model;
-        }
+        this.model = "default";
+    } else {
+        this.model = model;
+    }
         if (productionCountry == null) {
-            this.productionCountry = "default";
-        } else {
-            this.productionCountry = productionCountry;
-        }
+        this.productionCountry = "default";
+    } else {
+        this.productionCountry = productionCountry;
+    }
         if (engineVolume == 0) {
-            this.engineVolume = 1.5;
-        } else {
-            this.engineVolume = engineVolume;
-        }
+        this.engineVolume = 1.5;
+    } else {
+        this.engineVolume = engineVolume;
+    }
         if (color == null) {
-            this.color = "белый";
-        } else {
-            this.color = color;
-        }
+        this.color = "белый";
+    } else {
+        this.color = color;
+    }
         if (productionYear == 0) {
-            this.productionYear = 2020;
-        } else {
-            this.productionYear = productionYear;
-        }
+        this.productionYear = 2020;
+    } else {
+        this.productionYear = productionYear;
+    }
         if (key == null) {
-            this.key = new Key();
-        } else {
-            this.key = key;
-        }
+        this.key = new Key();
+    } else {
+        this.key = key;
+    }
         this.bodyType = "седан";
         this.numberOfPlaces = 5;
         this.transmission = "МКПП";
         this.registrationNumber = "x000xx000";
         this.summerTyres = true;
-    }
 
     public void printCar() {
         System.out.println(brand + " " + model + ", объем двигателя: " + engineVolume + ", цвет: " + color + ", год выпуска: " +
